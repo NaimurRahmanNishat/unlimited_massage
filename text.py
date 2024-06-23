bb3b7c563dc77abbef175a93c5e6ad0b6ef6d8c4
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-23 11:39:39 +0600

    update pyautogui

--- text.py
+++ text.py
@@ -1,7 +1,7 @@
-import pyautogui     # pip install pyautogui
+import pyautogui     # pip install pyautogui (py=python auto = auto gui = graphical user interface)
 import time
 while True:
-    time.sleep(0.1)
-    pyautogui.typewrite("Sorry?")
-    time.sleep(0.1)
+    time.sleep(0.0001)
+    pyautogui.typewrite("Babu rudro Ghum theke utho na...... Babu..")
+    time.sleep(0.0001)
     pyautogui.press('enter')
